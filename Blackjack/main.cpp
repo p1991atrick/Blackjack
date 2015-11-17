04df6a0a0d0f68f4fa94485c64191746cf60babb
Patrick <p1991atrick@gmail.com>
2015-11-17 10:45:38 -0800

    Initial Commit

---
+++ Blackjack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Blackjack
+//
+//  Created by Gordon Freeman on 11/17/15.
+//  Copyright © 2015 Patrick Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
